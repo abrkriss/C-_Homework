fb1d1e139ace737f7ccf92ac022779ebdd87c2b6
Kristina <abrkriss@gmail.com>
2022-11-18 23:50:10 +0300

    Exercize_2

---
+++ Exercize_2/Program.cs
@@ -0,0 +1,24 @@
+﻿void Exercize2()
+{
+//Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел..
+         
+          Console.WriteLine("Введите первое значение: ");
+          int a = Convert.ToInt32(Console.ReadLine());
+         
+          Console.WriteLine("Введите второе значение: ");
+          int b = Convert.ToInt32(Console.ReadLine());
+         
+          Console.WriteLine("Введите третье значение: ");
+          int c = Convert.ToInt32(Console.ReadLine());
+           
+           int max = a;
+
+          if (a > max) max = a;
+          if (b > max) max = b;
+          if (c > max) max = c;
+         
+           
+    Console.Write("max = ");
+    Console.WriteLine(max);
+}
+Exercize2();
