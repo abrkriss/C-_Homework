e70cabf1360b165619d53de48ddf42804e622838
Kristina <abrkriss@gmail.com>
2022-11-18 23:45:57 +0300

    Exercize_1

---
+++ Exercize_1/Program.cs
@@ -0,0 +1,17 @@
+﻿void Exercize1()
+{
+//Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+Console.WriteLine("Введите первое число: ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число: ");
+int numberB = Convert.ToInt32(Console.ReadLine());
+if (numberA > numberB)
+{
+    Console.WriteLine("Максимальное значение: " +  numberA );
+}
+else
+{
+    Console.WriteLine("Максимальное значение: " + numberB);
+}
+}
+Exercize1();
