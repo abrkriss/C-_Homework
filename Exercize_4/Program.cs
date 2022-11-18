2fa4de09da810aa06c5ea09bf8e1122d16c68613
Kristina <abrkriss@gmail.com>
2022-11-19 00:16:40 +0300

    Exercize_4

---
+++ Exercize_4/Program.cs
@@ -0,0 +1,22 @@
+﻿void Exercize4()
+{
+    //Напишите программу, которая на вход принимает число (N > 0), а на выходе показывает все чётные числа от 1 до N.
+
+    Console.WriteLine("Введите значение окончания исчисления на отрезке ");
+    int num = Convert.ToInt32(Console.ReadLine());
+        int result = 1;
+       
+        if (result == 1)
+
+        {
+         result += 1;
+        }
+
+        while (result <= num)
+
+        {
+            Console.WriteLine("Четные значения на отрезке: " + result);
+            result += 2;
+        }
+}
+Exercize4();
